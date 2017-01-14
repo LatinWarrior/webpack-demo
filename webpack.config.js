4b3000183cc8d7deacb4367b5d666f891e52a480
Luis <luis.f.blanco@gmail.com>
2017-01-14 16:36:17 -0600

    first check-in

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    entry: ['./utils', './app.js'],
+    output: {
+        filename: 'bundle.js'
+    },
+    //watch: true,
+    module: {
+        preloaders: [
+            {
+                test: /\.js$/,
+                exclude: 'node_modules',
+                loader: 'jshint-loader'
+            }
+        ],
+        loaders: [
+            {
+                test: /\.es6$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            }
+        ]
+    },
+
+    resolve: {
+        extensions: ['', '.js', '.es6']
+    }
+}
